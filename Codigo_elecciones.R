4ada9f46f01e5df39aeb9140ba6f3d3d7b22feab
David Humberto Jiménez Sánchez <dhumbertojs@gmail.com>
2019-05-06 23:13:11 -0500

    Actualizacion y corrección de base de datos

--- Codigo_elecciones.R
+++ Codigo_elecciones.R
@@ -1011,7 +1011,7 @@ Winner2 <- as.data.frame(Winner2)
 yuc15 <- bind_cols(yuc15, Winner2)
 rm(yuc, clave, gana, Winner2)
 
-####Data Lucardi####
+####Juntando la actualizacion####
 act <- bind_rows(list(nay14, bcs15, camp15, cdmx15, chis15, col15, gro15, gto15, jal15, mex15, mich15, mor15, nl15, qro15, slp15, son15, tab15, yuc15)) 
 suma <- act %>% select(-c("state", "year", "muni", "Winner2")) 
 suma <- suma %>% 
@@ -1031,18 +1031,27 @@ sum_pri <- suma %>% select(muniYear, starts_with("PRI"))
 sum_pri <- sum_pri %>% 
   mutate(s_PRI = rowSums(sum_pri[c("PRI_PVEM_PNA", "PRI_PVEM", "PRI", "PRI_PVEM_PNA_PD", "PRI_PVEM_PD","PRI_PNA_PD", "PRI_PNA", "PRI_PD", "PRI_PT", "PRI_PNA_PVEM", "PRI_PVEM_PT")], na.rm = T)) %>% 
   select(muniYear, s_PRI)
+colnames(sum_pri)[2] <- "PRI"
 
 sums <- full_join(sum_pan, sum_pri, by = "muniYear")
-rm(sum_pan, sum_pri)
+#rm(sum_pan, sum_pri)
 suma <- suma %>% 
-  select(muniYear, total) %>% full_join(sums, by = "muniYear")
+  select(muniYear, total) %>% full_join(sums)
 
 act <- act %>% 
-  select(muniYear, state, muni, year, Winner2, PAN, PRI, PRD, nulos, noreg) %>% 
-  right_join(suma)
-rm(nay14, bcs15, camp15, cdmx15, chis15, col15, gro15, gto15, jal15, mex15, mich15, mor15, nl15, qro15, slp15, son15, tab15, yuc15) 
-act <- act %>% mutate(muni = paste(state, muni, sep = ""))
+  select(muniYear, state, muni, year, Winner2, PAN, PRI, PRD, nulos, noreg)
 
+act <- bind_cols(act, suma)
+act <- act %>% 
+  mutate(
+    s_PAN = rowSums(act[c("PAN", "PAN1")], na.rm= T),
+    s_PRI = rowSums(act[c("PRI", "PRI1")], na.rm = T),
+    muni = paste(state, muni, sep = "")
+  ) %>% 
+  select(-c("PAN", "PAN1", "PRI", "PRI1", "muniYear1"))
+colnames(act)[11] <- "PAN"
+colnames(act)[12] <- "PRI"
+####Data Lucardi####
 data <- read.csv(paste(inp, "Municipal elections Mexico 1980-2013.csv", sep="/"))
 names(data) <- str_replace_all(names(data), "[.]", "_")
 
@@ -1051,10 +1060,8 @@ data <- data %>%
     muniYear = factor(substr(muniYear, 2, 11)),
     muni = factor(substr(muni, 2, 6)),
     state = ifelse(state=="AGS", "01", ifelse( state=="BC", "02", ifelse(state== "BCS", "03", ifelse(state=="CAM", "04", ifelse(state=="CHUA", "08", ifelse(state== "CHIA","07", ifelse(state=="CLA", "05", ifelse(state=="CMA", "06", ifelse(state=="DF", "09", ifelse(state=="DGO", "10", ifelse(state=="EDOMEX", "15", ifelse(state=="GTO", "11", ifelse(state== "GRO", "12", ifelse(state== "HID", "13", ifelse(state=="JAL", "14", ifelse(state== "MICH","16", ifelse(state=="MOR","17", ifelse(state=="NAY", "18", ifelse(state=="OAX", "20", ifelse(state=="NL", "19", ifelse(state== "PUE", "21", ifelse(state=="QUER", "22", ifelse(state=="QROO", "23", ifelse(state=="SLP", "24", ifelse(state== "SIN", "25", ifelse(state=="SON", "26", ifelse(state=="TAB", "27", ifelse(state=="TAM", "28", ifelse(state=="TLAX", "29", ifelse(state=="VER", "30", ifelse(state=="YUC", "31", "32" )))))))))))))))))))))))))))))))
-  ) #%>% select(muniYear, state, muni, year, Winner2, firstPRIdefeat)
-#data <- bind_rows(data, act)
-##NO OLVIDAR EL BIND
-#rm(act)
+  ) %>% select(muniYear, state, muni, year, Winner2, PAN, PRI, PRD, PAN_PRD, nulos, noreg, total)
+data <- bind_rows(data, act)
 
 data = arrange(data,muni)
 
@@ -1064,8 +1071,7 @@ data$Winner2 = str_replace_all(data$Winner2, 'Conv' , "MC")
 data$Winner2 = ifelse(data$Winner2 == "Independiente1", "INDEP1", data$Winner2)
 data$Winner2 = ifelse(data$Winner2 == "INDEP1", "INDEP", data$Winner2)
 
-##aquí me hace falta crear las dummies de concordancia partidista
-####Gubernaturas y concordancia####
+####Gubernaturas####
 gobs <- "https://docs.google.com/spreadsheets/d/e/2PACX-1vTAJ22OR9JCNfRdQFeHUO1szhw-tje4sjtMCMf3m9DA5mKjoWMNCWi4Pr3SPeO1TEb5wvvVZArw6ecq/pub?gid=1686758032&single=true&output=csv"
 gob <- read.csv(gobs, stringsAsFactors = F)
 gob$INEGI <- formatC(gob$INEGI, width = 2, format = "d", flag = "0")
@@ -1080,15 +1086,17 @@ for (x in 1:length(elec)) {
   setTxtProgressBar(pb, x)
   rm(tempo)
 }
+colnames(gober)[2] <- "win_state"
 gober <- gober %>% mutate(
   gub = paste(INEGI, year, sep = "_"),
-  win = sapply(strsplit(win, "_"), "[", 1)
+  wintop_state = sapply(strsplit(win_state, "_"), "[", 1)
   )
 
 data <- data %>% 
   mutate(gub = paste(state, year, sep = "_")
   ) %>% inner_join(gober, by = "gub") %>% select(-c(gub, INEGI, year.y))
 colnames(data)[4] <- "year"
+
 ####Subset de partidos####
 levels(as.factor(data$Winner2))     
 
@@ -1117,11 +1125,27 @@ data <- data %>%
                                           ifelse(inc == "PAN", "PAN",
                                                  #ifelse(inc == "MC", "MC", 
                                                         ifelse(inc == "PRD", "PRD", "Otros")))#))))
+    #firstPRIdefeat = ifelse(win_top != "PRI", year, NA)
+    #firstPRIdefeat = ifelse(win_top != "PRI", year, NA)
+  ) %>% 
+  ungroup(muni)
+####Concordancia y otras variables####
+data <- data %>% 
+  mutate(
+    conco = ifelse(win_top == wintop_state, 1, 0),
+    PRI_ofi = ifelse(win == "PRI" & wintop_state == "PRI", 1, 0),
+    PAN_ofi = ifelse(win == "PAN" & wintop_state == "PAN", 1, 0),
+    PRD_ofi = ifelse(win == "PRD" & wintop_state == "PRD", 1, 0),
+    PAN_PRD_ofi = ifelse(substr(Winner2, 1, 7) == "PAN_PRD" & substr(win_state, 1, 7) == "PAN_PRD", 1, 0),
+    PRI_opo = ifelse(win == "PRI" & wintop_state != "PRI", 1, 0),
+    PAN_opo = ifelse(win == "PAN" & wintop_state != "PAN", 1, 0),
+    PRD_ofpo= ifelse(win == "PRD" & wintop_state != "PRD", 1, 0),
+    PAN_PRD_opo = ifelse(substr(Winner2, 1, 7) == "PAN_PRD" & substr(win_state, 1, 7) != "PAN_PRD", 1, 0)
   )
 
 levels(as.factor(data$Winner2))
 levels(as.factor(data$win))
 summary(data)
-
+####Exportando a csv####
 as.data.frame(data, row.names = NULL)
 write.csv(data, paste(out, "data.csv", sep = "/"), row.names = F, fileEncoding ="UTF-8")
